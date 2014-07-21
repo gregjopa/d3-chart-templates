97d07f3f5fa6312566be0f68bad853b4509cb64e
Greg Jopa <gj870q@yp.com>
2014-07-21 11:06:07 -0700

    Add multi-line chart

--- src/js/multi_line_chart.js
+++ src/js/multi_line_chart.js
@@ -1,18 +1,19 @@
-var lineChart = function () {
+var multiLineChart = function () {
 
   // default values for configurable input parameters
 
   var data = [];
 
   // var data = [
-  //   {
-  //     'date': new Date('2014-01-01T08:00:00Z'),
-  //     'count': 26
-  //   },
-  //   {
-  //     'date': new Date(2014-01-02T08:00:00Z'),
-  //     'count': 43
-  //   }, ...
+  //   [
+  //     { 'date': new Date('2014-01-01T08:00:00Z'), 'count': 25 },
+  //     { 'date': new Date(2014-01-01T08:00:00Z'), 'count': 26 }
+  //   ],
+  //   [
+  //     { 'date': new Date('2014-01-02T08:00:00Z'), 'count': 37 },
+  //     { 'date': new Date(2014-01-02T08:00:00Z'), 'count': 41 }
+  //   ],
+  //   ...
   // ];
 
   var width = 422;
@@ -20,18 +21,20 @@ var lineChart = function () {
   var margin = { top: 20, right: 20, bottom: 50, left: 50 };
   var axisLabels = { x: 'Date', y: 'Count' };
   var xAxisDateFormat = d3.time.format('%d %b');
-  var color = '#1f77b4';
+  var colorScale = d3.scale.category20();
+  var legendLabels = ['Count 1', 'Count 2'];
+  var legendHeight = 60;
 
 
   var chart = function (container) {
 
     width = width - margin.left - margin.right;
-    height = height - margin.top - margin.bottom;
+    height = height - margin.top - margin.bottom - legendHeight;
 
 
     // setup x-axis
     var xScale = d3.time.scale()
-        .domain(d3.extent(data, function (d) { return d.date; }))
+        .domain(d3.extent(data, function (d) { return d[0].date; }))
         .range([20, width]);
 
     var xAxis = d3.svg.axis()
@@ -45,7 +48,10 @@ var lineChart = function () {
 
     // setup y-axis
     var yScale = d3.scale.linear()
-        .domain([0, d3.max(data, function (d) { return d.count; })])
+        .domain([0, d3.max(data, function (d) {
+          var counts = d.map(function (point) { return point.count; });
+          return Math.max.apply(Math, counts);
+        })])
         .range([height, 0]);
 
     var yAxis = d3.svg.axis()
@@ -57,25 +63,12 @@ var lineChart = function () {
         .tickFormat(d3.format('s'));
 
 
-    // setup line
-    var line = d3.svg.line()
-        .x(function (d) { return xScale(d.date); })
-        .y(function (d) { return yScale(d.count); });
-
-
-    // setup area
-    var area = d3.svg.area()
-        .x(function (d) { return xScale(d.date); })
-        .y0(height)
-        .y1(function (d) { return yScale(d.count); });
-
-
     // setup tooltip
     var tip = d3.tip()
         .attr('class', 'd3-tip')
         .html(function (d) {
           return '<span class="date">' + tooltipDateFormat(d.date) +
-            '</span><br><span class="circle" style="background-color: ' + color +
+            '</span><br><span class="circle" style="background-color: ' + d.color +
             ';"> </span>Clicks: ' + d.count;
         });
 
@@ -88,7 +81,7 @@ var lineChart = function () {
     var svg = container.append('svg')
         .attr('class', 'svg-chart line-chart')
         .attr('width', width + margin.left + margin.right)
-        .attr('height', height + margin.top + margin.bottom);
+        .attr('height', height + margin.top + margin.bottom + legendHeight);
 
 
     // draw x-axis
@@ -129,34 +122,47 @@ var lineChart = function () {
         .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
 
-    // draw line
-    g.append('path')
-        .attr('class', 'line')
-        .attr('d', line(data))
-        .style({ 'fill': 'none', 'stroke': color, 'stroke-width': 4 });
+    // setup and draw lines
+    data[0].forEach(function (point, i) {
 
+      var lines = {};
+      lines['line' + i] = d3.svg.line()
+          .x(function (d) { return xScale(d[i].date); })
+          .y(function (d) { return yScale(d[i].count); });
 
-    // draw area
-    g.append('path')
-        .datum(data)
-        .attr('class', 'area')
-        .attr('d', area)
-        .style({ 'fill': color, 'fill-opacity': 0.1 });
+      g.append('path')
+          .attr('class', 'line')
+          .attr('d', lines['line' + i](data))
+          .style({ 'fill': 'none', 'stroke': colorScale(i), 'stroke-width': 4 });
+
+    });
 
 
     // invoke tooltip
     g.call(tip);
 
 
+    // format data for drawing circles
+    var circleData = [];
+
+    data.forEach(function (points) {
+      points.forEach(function (point, i) {
+        point.color = colorScale(i);
+        circleData.push(point);
+      });
+    });
+
+
     // draw circles and add tooltip events
     g.selectAll('circle')
-        .data(data)
+        .data(circleData)
       .enter().append('circle')
         .attr('class', 'circle')
         .attr('cx', function (d) { return xScale(d.date); })
         .attr('cy', function (d) { return yScale(d.count); })
         .attr('r', 5)
-        .style({ 'fill': color, 'stroke': color })
+        .style('fill', function (d) { return d.color; })
+        .style('stroke', function (d) { return d.color; })
         .on('mouseover', function (d) {
           tip.show(d);
           d3.select(this).classed('active', true);
@@ -166,6 +172,38 @@ var lineChart = function () {
           d3.select(this).classed('active', false);
         });
 
+
+    // setup and draw legend
+    var legend = svg.append('g').attr('class', 'legend');
+
+    var previousTextLength = 0;
+    var legendGroup = legend.selectAll('circle')
+        .data(legendLabels)
+      .enter().append('g')
+        .attr('transform', function (d, i) {
+          var pixelWidth = previousTextLength * 12;
+          previousTextLength = d.length;
+          return 'translate(' + pixelWidth + ', 0)';
+        });
+
+    legendGroup.append('circle')
+        .attr('class', 'circle')
+        .attr('cx', 0)
+        .attr('cy', 5)
+        .attr('r', 5)
+        .style('fill', function (d, i) { return colorScale(i); });
+
+    legendGroup.append('text')
+        .attr('class', 'legend text')
+        .attr('x', 10)
+        .attr('y', 10)
+        .text(function (d) { return d; });
+
+    legend.attr('transform', function () {
+          var x = margin.left + width / 2 - this.getBBox().width / 2;
+          var y = height + margin.top + margin.bottom + legendHeight / 2;
+          return 'translate(' + x + ',' + y + ')';
+        });
   };
 
 
@@ -205,9 +243,21 @@ var lineChart = function () {
     return chart;
   };
 
-  chart.color = function (value) {
-    if (!arguments.length) return color;
-    color = value;
+  chart.colorScale = function (value) {
+    if (!arguments.length) return colorScale;
+    colorScale = value;
+    return chart;
+  };
+
+  chart.legendLabels = function (value) {
+    if (!arguments.length) return legendLabels;
+    legendLabels = value;
+    return chart;
+  };
+
+  chart.legendHeight = function (value) {
+    if (!arguments.length) return legendHeight;
+    legendHeight = value;
     return chart;
   };
 
